d7b0c24ec275875f0ecfd4e1c6a0d5d7a57cd6e2
Amulpreet Singh <amulpreetsingh1710@gmail.com>
2026-06-17 22:59:32 +0530

    Create counting-inversions.cpp
    1.) i < j
    2.) arr[i] > arr[j]
    
    merge sort deep understanding

---
+++ counting-inversions.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int merge(vector<int> &arr,int st,int mid, int end){
+    vector<int> temp;
+    int i = st, j = mid+1;
+    int invCount = 0;
+
+    while(i<= mid && j<= end){
+        if(arr[i] <= arr[j]){
+            temp.push_back(arr[i]);
+            i++;
+        }
+        else{
+            temp.push_back(arr[j]);
+            j++;
+            invCount += (mid - i+1);
+        }
+    }
+    while(i <= mid){
+        temp.push_back(arr[i]);
+            i++;
+    }
+    while(j <= end){
+        temp.push_back(arr[j]);
+            j++;
+    }
+
+    for(int idx = 0; idx < temp.size(); idx++){
+        arr[idx+st]= temp[idx];
+    }
+    return invCount;
+}
+
+int mergesort(vector<int> &arr,int st, int end){
+    if(st < end){
+        int mid = st + (end - st)/2;
+
+        int leftInvCount = mergesort(arr,st,mid);
+        int rightInvCount = mergesort(arr,mid+1,end);
+
+        int invCount = merge(arr,st,mid,end);
+
+        return invCount + rightInvCount + leftInvCount; 
+    }
+    return 0;
+
+}
+
+int main(){
+    vector<int> arr = {6,3,5,2,7};
+    // vector <int> arr = {1,3,5,10,2,6,8,9}
+
+    int ans = mergesort(arr,0,arr.size()-1);
+    cout<< "inv count : " <<  ans<< endl;
+
+
+    return 0;
+}
