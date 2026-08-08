03f97c48aa161034aff12de30b2e3b1b897a170b
Amulpreet Singh <amulpreetsingh1710@gmail.com>
2026-08-08 16:38:57 +0530

    Time: 4 ms (8.81%) | Memory: 27.1 MB (34.50%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,53 +1,26 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        stack <int> s;
         int n = height.size();
-        vector<int> right(n,0);
+        vector<int> rmax(n,0);
+        vector<int> lmax(n,0);
 
-        for(int i = n-1;i >= 0;i--){
-            while(s.size() != 0 && s.top() <= height[i]){
-                s.pop();
-            }
-            right[i] = s.size() == 0 ? -1 : s.top();
+        rmax[n-1] = height[n-1];
+        lmax[0] = height[0];
 
-            if(s.size() != 0 && height[i] < s.top()){
-                continue;
-            }
-            s.push(height[i]);
+        for(int i = n-2;i >= 0;i--){
+            rmax[i] = max(rmax[i+1], height[i]);
         }
 
-        while(s.size()!= 0){
-            s.pop();
+        for(int i = 1; i< n; i++){
+            lmax[i] = max(lmax[i-1], height[i]);
         }
 
-
-        vector<int> left(n,0);
-
-        for(int i = 0; i< n; i++){
-
-            while(s.size() != 0 && s.top() <= height[i]){
-                s.pop();
-            }
-            left[i] = s.size() == 0 ? -1 : s.top();
-
-            if(s.size() != 0 && height[i] < s.top()){
-                continue;
-            }
-            s.push(height[i]);
-        }
 
         int area = 0;
-        for(int i= 0; i< n ;i++){
-            if(right[i] == -1 || left[i] == -1){
-                continue;
-            }
-            else if(right[i] > left[i]){
-                area += left[i] - height[i];
-            }
-            else{
-                area += right[i] - height[i];
-            }
+        for(int i= 0; i< n;i++){
+            
+            area += min(rmax[i],lmax[i]) - height[i];
             
         }
         return area;
