062c1559dc59d87840876c1a74581a3795870e24
Amulpreet Singh <amulpreetsingh1710@gmail.com>
2026-09-19 02:28:46 +0530

    Time: 54 ms (47.70%) | Memory: 19.4 MB (41.26%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,46 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map <char,int> m;
-        int max_sub = 0;
-        int l = 0,r = 0;
+        unordered_map<char, int> m;
+        int max_sub = 0, l = 0;
         
-        for(int i = 0; i< s.size(); i++){
-            if(m.find(s[i]) != m.end()){
-                r = i;
-                max_sub = max(max_sub,r-l);
-                l = m[s[i]] + 1;
-                m[s[i]] = i;
-                r++;
-                break;
+        for (int r = 0; r < s.size(); r++) {
+            // If the character is found inside our current active window, jump 'l'
+            if (m.find(s[r]) != m.end() && m[s[r]] >= l) {
+                l = m[s[r]] + 1;
             }
-            else{
-                m[s[i]] = i;
-            }
+            m[s[r]] = r;
+            max_sub = max(max_sub, r - l + 1);
         }
-
-        if(r == 0){
-            return s.size();
-        }
-        while(r < s.size()){ 
-            if(m.find(s[r]) != m.end()){
-                if(m[s[r]] < l){
-                   max_sub = max(max_sub, r -l + 1);
-                   m[s[r]] = r; 
-                }
-                else{
-                    l = m[s[r]] + 1;
-                    m[s[r]] = r; 
-                }
-                
-            }
-            else{
-                m[s[r]] = r;
-                max_sub = max(max_sub, r -l + 1);
-            }
-            r++;
-        }
-
+        
         return max_sub;
     }
 };
